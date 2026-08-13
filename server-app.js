faacc1447f15970e099934ff7eeb79a647dfb0bb
yh169499-rgb <yh169499@gmail.com>
2026-08-13 15:17:05 +0800

    chore: remove temporary production migration endpoint

--- server-app.js
+++ server-app.js
@@ -17,7 +17,6 @@ const attendanceRoutes = require('./routes/attendance');
 const workforceReportRoutes = require('./routes/workforce-reports');
 const directoryRoutes = require('./routes/directory');
 const { createAiReportRouter } = require('./routes/ai-reports');
-const { createDeploymentRouter } = require('./routes/deployment');
 
 function createServerApp(options = {}) {
   const app = express();
@@ -64,7 +63,6 @@ function createServerApp(options = {}) {
   app.use('/api', attendanceRoutes);
   app.use('/api', workforceReportRoutes);
   app.use('/api', createAiReportRouter(options.aiReport));
-  app.use('/api', createDeploymentRouter(options.deployment));
   app.get('/api/health', (_req, res) => {
     res.json({ ok: true, service: 'property-oa' });
   });
